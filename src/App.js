b3472d7be3f4baec87d15822aacd8d79aebc42e3
Ahlem Ahmed <ahmedahlem3@gmail.com>
2021-03-12 22:32:50 +0100

    finished the checkpoint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+// import ReactPlayer from 'react-player';
+import './style.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div style={{border:"'solid' 1 'black'",maxWidth:'100vw'}}>
+     <h1 className='title red'>AHMED Ahlem</h1> <br/>
+      <img src={logo} width="400" alt='logo in src'/> <br/>
+      <img src="logo192.png" alt='public logo'/>
+      </div>
+      <iframe src="https://www.youtube.com/embed/zTitoHKsyJg"  width="320" height="240" title >
+      </iframe>
     </div>
   );
 }
